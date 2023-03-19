74966d15e607d0a978f601c1f4fe33e67589ff79
Tushar-Kanti-Dev <62166306+Tushar-Kanti-Dev@users.noreply.github.com>
2023-03-19 17:34:17 +0600

    try to responsive in css3 update data picture

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Service.css'
 
 const Service = ({service}) => {
     const {name, id, desc, img, price} = service;
     return (
-        <div>
+        <div className='service-container'>
             <img src={img} alt="" />
             <h2>{name}</h2>
+            <p>Price: ${price}</p>
+            <p><small>{desc}</small></p>
+            <button className='book-button'>BOOK:{name}</button>
         </div>
     );
 };
